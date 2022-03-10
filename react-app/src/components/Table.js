d87be448d6074b34226765ef9a841400978fe1a4
DenxyChen <denxychen@gmail.com>
2022-03-10 14:17:25 -0800

    edit page navigation working

--- react-app/src/components/Table.js
+++ react-app/src/components/Table.js
@@ -6,12 +6,12 @@ import TableHead from './TableHead';
 import Row from './Row';
 
 
-export default function Table({ exercises, onDelete }) {
+export default function Table({ exercises, onDelete, onEdit }) {
     return (
         <table>
             <TableHead />
             <tbody>
-                {exercises.map((exercise, i) => <Row exercise={exercise} onDelete={onDelete} key={i} />)}
+                {exercises.map((exercise, i) => <Row exercise={exercise} onDelete={onDelete} onEdit={onEdit} key={i} />)}
             </tbody>
         </table>
     );
